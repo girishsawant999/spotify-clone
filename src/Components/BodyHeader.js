ad61f976a3aebf73c0423b19767f25a2f5fbc83e
Girish <girishsawant999.gs@gmail.com>
2020-08-15 13:43:32 +0530

    Menu button changes

--- src/Components/BodyHeader.js
+++ src/Components/BodyHeader.js
@@ -1,26 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './body.css';
 import { useDataLayerValue } from '../DataLayer';
 import SearchIcon from '@material-ui/icons/Search';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 function BodyHeader(props) {
   const [{ user }, dispatch] = useDataLayerValue();
+  const [menuOpen, setmenuOpen] = useState(false);
   useEffect(() => {
     const menuBtn = document.getElementById('menuBtn');
     menuBtn.addEventListener('click', () => {
-      if(document.getElementById('sidebar').style.display === 'none') {
+      if (document.getElementById('sidebar').style.display === 'none') {
         document.getElementById('sidebar').style.display = 'block';
         document.getElementById('searchInput').style.display = 'none';
         document.getElementById('header__user').style.display = 'none';
+        setmenuOpen(true);
       } else {
         document.getElementById('sidebar').style.display = 'none';
         document.getElementById('searchInput').style.display = 'flex';
         document.getElementById('header__user').style.display = 'flex';
-
+        setmenuOpen(false);
       }
-
     });
     return () => {
       menuBtn.removeEventListener('click', {});
@@ -30,7 +32,7 @@ function BodyHeader(props) {
   return (
     <div className="body__header">
       <span id="menuBtn" className="body__menu">
-        <MenuIcon />
+        {menuOpen ? <CloseIcon /> : <MenuIcon />}
       </span>
       <div id="searchInput" className="body__search">
         <SearchIcon className="body__SearchIcon" />
