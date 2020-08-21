6782e97248bf86369604a81a14b48cec9c4849b1
Girish <girishsawant999.gs@gmail.com>
2020-08-21 22:11:59 +0530

    Routes in sidebar

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './player.css';
-import Sidebar from './Sidebar';
 import Footer from './Footer';
 import Routes from '../Routes';
 
@@ -8,7 +7,6 @@ function Player(props) {
   return (
     <div className="player">
       <div className="player_body">
-        <Sidebar />
         <Routes />
       </div>
       <Footer />
--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Body from './Components/Body';
 import PlayListView from './Components/PlayListView';
+import Sidebar from './Components/Sidebar';
 
 function Routes(props) {
   return (
     <Router>
+      <Sidebar />
       <Switch>
         <Route exact path="/" component={Body} />
         <div class="routes">
