7829134822a672ca719ebed3756d892ff06d6f5f
Girish <girishsawant999.gs@gmail.com>
2020-11-13 23:09:15 +0530

    Play button on recent tracks

--- src/Components/TrackCards/TrackCardZero.js
+++ src/Components/TrackCards/TrackCardZero.js
@@ -1,33 +1,15 @@
 import PauseIcon from "@material-ui/icons/Pause";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { useDataLayerValue } from "../../DataLayer";
 import "./trackCard.css";
 
 function TrackCard({ track, trackIndex }) {
   const [{ currentTrack, play }, dispatch] = useDataLayerValue();
+  const [showButton, setshowButton] = useState(false);
 
-  const initilize = (params) => {
-    document.getElementById(trackIndex).addEventListener("mouseover", (e) => {
-      if (document.getElementById(`play-${trackIndex}`))
-        document.getElementById(`play-${trackIndex}`).style.opacity = 1;
-    });
-
-    document.getElementById(trackIndex).addEventListener("mouseleave", (e) => {
-      if (document.getElementById(`play-${trackIndex}`))
-        document.getElementById(`play-${trackIndex}`).style.opacity = 0;
-    });
-    return () => {
-      document.removeEventListener("mouseover", (e) => {});
-      document.removeEventListener("mouseleave", (e) => {});
-    };
-  };
-
-  useEffect(initilize, []);
-
-  const playTrack = (event, track) => {
-    event.preventDefault();
+  const playTrack = (track) => {
     dispatch({
       currentTrack: {
         trackUrl: track?.track?.preview_url,
@@ -44,17 +26,24 @@ function TrackCard({ track, trackIndex }) {
   };
 
   return (
-    <div id={trackIndex} className="trackcard">
+    <div
+      id={trackIndex}
+      className="trackcard"
+      onMouseOver={() => setshowButton(true)}
+      onMouseLeave={() => setshowButton(false)}
+    >
       <LazyLoadImage src={track?.track?.album?.images[1]?.url} />
       <h4>{track?.track?.name}</h4>
       <p>{track?.track?.artists.map((artist) => artist.name).join(", ")}</p>
-      <span id={`play-${trackIndex}`} className="playIcon">
-        {play && currentTrack?.trackUrl === track?.track?.preview_url ? (
-          <PauseIcon onClick={() => pauseTrack(track)} />
-        ) : (
-          <PlayArrowIcon onClick={(e) => playTrack(e, track)} />
-        )}
-      </span>
+      {(showButton || currentTrack?.trackUrl === track?.track?.preview_url) && (
+        <span id={`play-${trackIndex}`} className="playIcon">
+          {play && currentTrack?.trackUrl === track?.track?.preview_url ? (
+            <PauseIcon onClick={() => pauseTrack(track)} />
+          ) : (
+            <PlayArrowIcon onClick={(e) => playTrack(track)} />
+          )}
+        </span>
+      )}
     </div>
   );
 }
