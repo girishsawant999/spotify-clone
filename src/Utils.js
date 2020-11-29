41544121791f3292396f8bef06c8110ceb157310
Girish <girishsawant999.gs@gmail.com>
2020-11-29 20:13:27 +0530

    Changes for localhost incompatibility with app

---
+++ src/Utils.js
@@ -0,0 +1,10 @@
+export const checkLocalStorageCompatibility = () => {
+  const test = "test";
+  try {
+    localStorage.setItem(test, test);
+    localStorage.removeItem(test);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
